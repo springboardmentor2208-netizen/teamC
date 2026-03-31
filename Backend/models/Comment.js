55d832ba29644188bfba2e7c767665367bb53d3f
sanikadali <sanikadali70@gmail.com>
2026-03-31 11:02:46 +0530

    Removed

--- Backend/models/Comment.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const commentSchema = new mongoose.Schema({
-    user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    complaint_id: { type: mongoose.Schema.Types.ObjectId, ref: "Complaint" },
-    content: String
-}, { timestamps: true });
-
-module.exports = mongoose.model("Comment", commentSchema);
